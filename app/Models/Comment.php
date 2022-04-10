e85d2c2752e4b44ec425f44fc5c4e89534ce0609
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-10 21:20:11 +0800

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -20,4 +20,9 @@ class Comment extends Model
     {
         return $this->belongsTo(Post::class);
     }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
