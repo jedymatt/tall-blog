9ada6ba3217111d651a3565febcd7f22229e1c8a
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-24 16:06:19 +0800

    Added index method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function index()
+    {
+        $posts = Post::paginate(10);
+        return view('posts.index', compact('posts'));
+    }
+
     public function show(Post $post)
     {
         return view('posts.show', compact('post'));
