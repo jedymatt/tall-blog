3cb84964df3ac1c59f449d9fda40227755cf10d8
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-13 19:59:14 +0800

    Create PostFactory.php

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = $this->faker->words(asText: true);
+
+        return [
+            'title' => Str::title($title),
+            'slug' => Str::slug($title),
+            'content' => $this->faker->paragraphs(asText: true),
+        ];
+    }
+}
