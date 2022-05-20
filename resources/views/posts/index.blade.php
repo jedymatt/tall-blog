a64bbdd42edb3d48fbd6093ed605d2fdc759c6be
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-20 16:48:37 +0800

    Fix layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,20 +1,22 @@
 <x-app-layout>
     <div class="py-12">
-       <div class="px-6">
-           @foreach ($posts as $post)
-               <div class="m-4">
-                   <div class="border rounded-md shadow-sm p-4">
-                       <h2 class="text-xl font-medium">{{ $post->title }}</h2>
-                       <p class="indent-5 mt-1">
-                           {{ $post->content }}
-                       </p>
-                       <div class="mt-1">
-                           <a href="{{ route('posts.show', $post) }}" class="hover:cursor-pointer">Read more...</a>
-                       </div>
-                   </div>
-               </div>
-           @endforeach
-           {{ $posts->links() }}
-       </div>
+        <div class="px-6">
+            @foreach ($posts as $post)
+                <div class="m-4">
+                    <div class="border rounded-md shadow-sm p-4">
+                        <h2 class="text-xl font-medium">{{ $post->title }}</h2>
+                        <p class="indent-5 mt-1">
+                            {{ $post->content }}
+                        </p>
+                        <div class="mt-1">
+                            <a href="{{ route('posts.show', $post) }}" class="hover:cursor-pointer">Read more...</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+            <div class="m-4">
+                {{ $posts->links() }}
+            </div>
+        </div>
     </div>
 </x-app-layout>
