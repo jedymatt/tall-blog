cf9274045f5a0b8a3037b0e15d77a9bafc037d92
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-20 16:50:51 +0800

    Create StorePostRequest.php

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|unique:posts',
+            'slug' => 'required|string|unique:posts',
+            'content' => 'required|string'
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'slug' => Str::slug($this->title),
+        ]);
+    }
+}
