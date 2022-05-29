dfc9c3e1e881a20b76f6ef70b22d6043fa4376d3
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-29 12:35:20 +0800

    Added login, logout and register links in navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,13 +3,18 @@
     <a href="/" class="text-xl font-bold">{{ config('app.name') }}</a>
 
     <div class="ml-auto">
-        <a href="https://www.github.com/jedymatt/tall-blog" target="_blank" class="flex items-center justify-center">
-            Github
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path
-                    d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"/>
-                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"/>
-            </svg>
-        </a>
+        @guest
+            <a href="{{ route('login') }}" class="text-sm text-gray-600 hover:text-gray-900">Login</a>
+            <a href="{{ route('register') }}" class="text-sm text-gray-600 hover:text-gray-900">Register</a>
+        @endguest
+        @auth
+            <a href="{{ route('logout') }}" class="text-sm text-gray-600 hover:text-gray-900"
+                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        @endauth
     </div>
 </nav>
