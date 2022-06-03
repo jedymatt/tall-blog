bef8102a37e93c4854bf5c49e2f2745650b12d4a
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-03 11:12:11 +0000

    Added user and publish

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,10 +14,36 @@ class Post extends Model
         'title',
         'slug',
         'content',
+        'user_id',
     ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function publish(): void
+    {
+        $this->published_at = now();
+        $this->save();
+    }
+
+    public function isPublished(): bool
+    {
+        return $this->published_at !== null;
+    }
+
+    public function isDraft(): bool
+    {
+        return $this->published_at === null;
     }
 }
