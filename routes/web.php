8bcd720ce30c330bb61c253b4957a810d75ced4b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-03 11:09:12 +0000

    Added home route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('posts.index');
 });
+
+Route::get('/home', [HomeController::class, 'show'])
+    ->middleware('auth')
+    ->name('home');
+
 
 Route::get('/posts', [PostController::class, 'index'])
     ->name('posts.index');
