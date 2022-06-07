1cbf8351c343414120c195789e95011aeea0bb0d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-07 15:53:15 +0000

    Added rules in WritePostRequest

--- app/Http/Requests/User/WritePostRequest.php
+++ app/Http/Requests/User/WritePostRequest.php
@@ -13,7 +13,7 @@ class WritePostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class WritePostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|string',
+            'body' => 'required|string',
         ];
     }
 }
