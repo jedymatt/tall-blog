efc6a0a94a31831861377f0f58028a00b076a025
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-11 22:35:26 +0800

    Use tinymce text editor

--- resources/views/user/write-post.blade.php
+++ resources/views/user/write-post.blade.php
@@ -3,20 +3,25 @@
         <form method="POST" action="{{ route('home.write-post') }}">
             @csrf
             <div class="flex  items-center justify-between">
-            <h1>Write a Post</h1>
-            <x-btn.primary type="submit">Save Post</x-btn.primary>
+                <h1>Write a Post</h1>
+                <x-btn.primary type="submit">Save Post</x-btn.primary>
             </div>
-            <div class="">
-                <label>
-                    <input type="text" class="text-3xl font-bold border-transparent focus:border-transparent focus:ring-transparent placeholder:text-gray-400" name="title" placeholder="Title">
-                </label>
+            <div>
+                <textarea class="resize-y w-full text-3xl font-bold border-transparent focus:border-transparent focus:ring-transparent placeholder:text-gray-400" name="title" placeholder="Title" rows="1"></textarea>
             </div>
-            <div class="form-group">
-                <label for="body">Body:</label>
-                <textarea class="form-control" id="body" name="body"></textarea>
-                            </div>
-
+            <div>
+                <textarea id="body" name="body"></textarea>
+            </div>
         </form>
     </div>
-
 </x-app-layout>
+<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    tinymce.init({
+        selector: 'textarea#body',
+        plugins: 'autoformat code',
+        toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify',
+        menubar: false,
+        statusbar: false,
+    });
+</script>
