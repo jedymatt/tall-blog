b211f8c82128ba6061a6857c7d81acf02f3e46cf
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-11 22:35:00 +0800

    Use {!! !!} in $post->body

--- resources/views/user/my-posts.blade.php
+++ resources/views/user/my-posts.blade.php
@@ -1,10 +1,12 @@
 <x-app-layout>
-    <div>
+    <div class="py-12 px-6">
         @foreach ($posts as $post)
+        <div class="first:mt-0 mt-4">
             <article>
                 <h2>{{ $post->title }}</h2>
-                <p>{{ $post->body }}</p>
-        </article>
+                <p>{!! $post->body !!}</p>
+            </article>
+        </div>
         @endforeach
     </div>
 </x-app-layout>
