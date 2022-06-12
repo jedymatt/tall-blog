e0fe40c81af819a3bf79171ccd5117cc07cea3a4
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-12 18:55:14 +0800

    Update routes

--- routes/user.php
+++ routes/user.php
@@ -1,13 +1,19 @@
 <?php
-use App\Http\Controllers\User\PostController;
-use App\Http\Controllers\User\WrittenPostController;
 
-Route::prefix('home')->name('home.')->middleware('auth')->group(function () {
-    Route::get('/my-posts', [PostController::class , 'index'])
+use App\Http\Controllers\User\WrittenPostController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\HomeController;
+
+Route::prefix('home')->middleware('auth')->group(function () {
+    Route::get('/', [HomeController::class, 'show'])
+        ->middleware('auth')
+        ->name('home');
+
+    Route::get('/my-posts', [WrittenPostController::class, 'index'])
         ->name('my-posts');
 
-    Route::get('/write-post', [WrittenPostController::class , 'create'])
+    Route::get('/write-post', [WrittenPostController::class, 'create'])
         ->name('write-post');
 
-    Route::post('/write-post', [WrittenPostController::class , 'store']);
+    Route::post('/write-post', [WrittenPostController::class, 'store']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,11 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('posts.index');
 });
-
-Route::get('/home', [HomeController::class, 'show'])
-    ->middleware('auth')
-    ->name('home');
-
 
 Route::get('/posts', [PostController::class, 'index'])
     ->name('posts.index');
