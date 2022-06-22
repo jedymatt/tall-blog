5511614f1152308916d91d2f7f0e3203a1da8ceb
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-23 00:18:45 +0800

    Change background color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,10 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -15,13 +16,15 @@
 
     @livewireStyles
 </head>
+
 <body class="antialiased">
-<div class="min-h-screen">
-    @include('layouts.navigation')
-    <div>
-        {{ $slot }}
+    <div class="min-h-screen bg-slate-50">
+        @include('layouts.navigation')
+        <div>
+            {{ $slot }}
+        </div>
     </div>
-</div>
-@livewireScripts
+    @livewireScripts
 </body>
+
 </html>
