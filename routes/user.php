4298ce44a1721fcb10805fb371824da781fdd111
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-22 23:40:53 +0800

    Remove home prefix and duplate home route

--- routes/user.php
+++ routes/user.php
@@ -4,10 +4,7 @@ use App\Http\Controllers\User\WrittenPostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\HomeController;
 
-Route::prefix('home')->middleware('auth')->group(function () {
-    Route::get('/', [HomeController::class, 'show'])
-        ->middleware('auth')
-        ->name('home');
+Route::middleware('auth')->group(function () {
 
     Route::get('/my-posts', [WrittenPostController::class, 'index'])
         ->name('my-posts');
