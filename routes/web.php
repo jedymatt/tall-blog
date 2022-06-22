d7c2446d62dd4ab203dd216264088bc71797d562
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-22 23:40:22 +0800

    Display home view from route /

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('posts.index');
-});
+    $posts = Post::published()->latest()->paginate();
+
+    return view('home', compact('posts'));
+})
+    ->name('home');
 
 Route::get('/posts', [PostController::class, 'index'])
     ->name('posts.index');
