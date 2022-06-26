21aba6182026cc91ff18098f9173d20417509ab2
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-26 23:30:57 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <div class="py-12 px-6">
-        <div class="grid gap-2 place-items-center">
+        <div class="grid gap-6 place-items-center">
             @foreach ($posts as $post)
-                <div class="w-2/3">
+                <div>
                     <x-post-card :post="$post" />
                 </div>
             @endforeach
