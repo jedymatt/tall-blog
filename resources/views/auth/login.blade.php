9b219f20c1f3a9b2fd6f5425801ca60f8c3ca103
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-26 23:30:45 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,39 @@
 <x-app-layout>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+    <div class="py-12 px-4">
+        <div class="flex flex-col justify-center items-center">
+            <div class="w-full max-w-md rounded-md shadow-md p-4 bg-white">
+                @if ($errors->any())
+                    <div>
+                        <div class="font-medium text-red-600">
+                            {{ __('Whoops! Something went wrong.') }}
+                        </div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
-
-                            <div>
-                                <label for="email">{{ __('E-Mail Address') }}</label>
-
-                                <div>
-                                    <input id="email" type="email" class="border-gray-400 rounded-md focus:border-indigo-500  focus:ring-indigo-500 focus:ring-1" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div>
-                                <label for="password">{{ __('Password') }}</label>
-
-                                <div class="">
-                                    <input id="password" type="password" class="border-gray-400 rounded-md focus:border-indigo-500  focus:ring-indigo-500 focus:ring-1" name="password" required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div>
-                                <div>
-                                    <label>
-                                        <input class="rounded-md" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <x-btn.primary type="submit">
-                                        {{ __('Login') }}
-                                    </x-btn.primary>
-
-                                    @if (Route::has('password.request'))
-                                    <a href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                    @endif
-                                </div>
-                        </form>
+                        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                </div>
+                @endif
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <label>
+                        Email
+                        <input class="mt-1 w-full form-input" type="email" name="email" :value="old('email')" required
+                            autofocus />
+                    </label>
+
+                    <div class="mt-4">
+                        <label>
+                            Password
+                            <input class="mt-1 w-full form-input" type="password" name="password" required />
+                        </label>
+                    </div>
+                    <div class="mt-4 flex justify-end">
+                        <button class="form-btn" type="submit">Login</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-
 </x-app-layout>
