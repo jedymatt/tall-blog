876303989ccf7d6da81c15f8655441289aa3cbbf
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-27 17:51:24 +0800

    Create UpdatePersonalInformation.php

---
+++ app/Http/Livewire/User/Profile/UpdatePersonalInformation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\Profile;
+
+use Livewire\Component;
+
+class UpdatePersonalInformation extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.profile.update-personal-information');
+    }
+}
