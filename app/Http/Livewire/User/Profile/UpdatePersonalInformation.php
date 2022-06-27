9ae06de102ff796dfc57f94cce7ecb11d54e6c2d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-28 00:32:00 +0800

    Implement functional update personal information

--- app/Http/Livewire/User/Profile/UpdatePersonalInformation.php
+++ app/Http/Livewire/User/Profile/UpdatePersonalInformation.php
@@ -2,12 +2,42 @@
 
 namespace App\Http\Livewire\User\Profile;
 
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class UpdatePersonalInformation extends Component
 {
+    public $user;
+
+    public function mount()
+    {
+        $this->user = auth()->user()->toArray();
+    }
+
+
     public function render()
     {
         return view('livewire.user.profile.update-personal-information');
+    }
+
+    public function update()
+    {
+        $this->validate(
+            [
+                'user.name' => ['required', 'string'],
+                'user.email' => ['required', 'email', Rule::unique('users', 'email')->ignore($this->user['id'])],
+            ],
+            attributes: [
+                'user.name' => 'name',
+                'user.email' => 'email',
+            ]
+        );
+
+        auth()->user()->update([
+            'name' => $this->user['name'],
+            'email' => $this->user['email'],
+        ]);
+
+        session()->flash('success', 'Updated!');
     }
 }
