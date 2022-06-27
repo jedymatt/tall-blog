0e39d321029de3d1c315349d44d86efbfeaa665c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-28 00:31:31 +0800

    Update update-personal-information.blade.php

--- resources/views/livewire/user/profile/update-personal-information.blade.php
+++ resources/views/livewire/user/profile/update-personal-information.blade.php
@@ -1,3 +1,25 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="shadow p-4 bg-white sm:rounded-md">
+    <div class="flex flex-col gap-4">
+        <label>
+            Name
+            <input class="form-input" type="text" wire:model.defer="user.name">
+            @error('user.name')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
+        </label>
+
+        <label>
+            Email
+            <input class="form-input" type="email" wire:model.defer="user.email">
+            @error('user.email')
+                <span class="text-red-500">{{ $message }}</span>
+            @enderror
+        </label>
+    </div>
+    <div class="mt-4 flex justify-end w-full">
+        @if (session('success'))
+            {{ session('success') }}
+        @endif
+        <button wire:click="update" class="form-btn">Save</button>
+    </div>
 </div>
