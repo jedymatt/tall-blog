ef7363927c51735f549d3783931ae80b8c8ea817
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-27 11:40:12 +0800

    Update profile.blade.php

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,28 +1,53 @@
 <x-app-layout>
     <div class="py-12 px-4">
         <div class="flex justify-center">
-            <div class="max-w-3xl w-full rounded-md border p-4 bg-white shadow-sm">
-                <div class="grid grid-cols-3 grid-flow-row gap-y-4">
-                    <div>
-                        Name
-                    </div>
-                    <div class="col-span-2">
-                        <input class="form-input" type="text" value="{{ $user->name }}">
+            <div class="max-w-3xl w-full flex flex-col gap-4">
+                <div class="rounded-md border p-4 bg-white shadow-sm">
+                    <div class="grid grid-cols-3 grid-flow-row gap-4">
+                        <div class="">
+                            Personal Information
+                        </div>
+                        <div class="col-span-2 flex flex-col gap-4">
+                            <label>
+                                <span class="text-sm font-semibold">Name</span>
+                                <input class="form-input" type="text" value="{{ $user->name }}">
+                            </label>
+
+                            <label>
+                                <span class="text-sm font-semibold">Email</span>
+                                <input class="form-input" type="email" value="{{ $user->email }}">
+                            </label>
+                        </div>
+                        <div class="col-span-3 flex justify-end w-full">
+                            <button class="form-btn">Save</button>
+                        </div>
                     </div>
 
-                    <div>
-                        Email
-                    </div>
-                    <div class="col-span-2">
-                        <input class="form-input" type="email" value="{{ $user->email }}">
+                    <div class="border-t mt-4 pt-4 grid grid-cols-3 grid-flow-row gap-4">
+                        <div class="">
+                            Security
+                        </div>
+                        <div class="col-span-2 flex flex-col gap-2">
+                            <div>
+                                <label>
+                                    <span class="text-sm font-semibold">Current Password</span>
+                                    <input class="form-input" type="password" placeholder="Current Password">
+                                </label>
+                            </div>
+                            <div class="mt-4">
+                                <label>
+                                    <span class="text-sm font-semibold">New Password</span>
+                                    <input class="form-input" type="password" placeholder="New Password">
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-span-3 flex justify-end w-full">
+                            <button class="form-btn">Update Password</button>
+                        </div>
                     </div>
 
-                </div>
-                <div class="mt-4 flex justify-end w-full">
-                    <button class="form-btn">Save</button>
                 </div>
             </div>
-
         </div>
 
     </div>
