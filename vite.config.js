d9b1fe47bc8d7c097421236a5987bee7f3bd5561
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-01 22:21:21 +0800

    Update config refresh browser when blade file changes

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,17 @@ export default defineConfig({
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
-        ])
+        ]),
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        },
     ],
 });
