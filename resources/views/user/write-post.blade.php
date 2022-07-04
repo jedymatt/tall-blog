408adbcfcf72f4988984d9bfc0f6df8bbb48c5eb
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-04 14:22:19 +0800

    Update write-post.blade.php

--- resources/views/user/write-post.blade.php
+++ resources/views/user/write-post.blade.php
@@ -2,26 +2,20 @@
     <div class="px-4 py-6">
         <form method="POST" action="{{ route('write-post') }}">
             @csrf
-            <div class="flex  items-center justify-between">
-                <h1>Write a Post</h1>
-                <x-btn.primary type="submit">Save Post</x-btn.primary>
+            <div class="flex items-center justify-between">
+                <h1 class="text-3xl font-semibold">Write a Post</h1>
+                <div class="flex flex-row gap-2">
+                    <button class="p-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-800 text-sm transition-colors"
+                        type="submit">Save as Draft</button>
+                </div>
             </div>
-            <div>
-                <textarea class="resize-y w-full text-3xl font-bold border-transparent focus:border-transparent focus:ring-transparent placeholder:text-gray-400" name="title" placeholder="Title" rows="1"></textarea>
-            </div>
-            <div>
-                <textarea id="body" name="body"></textarea>
-            </div>
+            <textarea
+                class="mt-4 resize-y w-full text-3xl font-bold border-transparent focus:border-transparent focus:ring-transparent placeholder:text-gray-400"
+                name="title" placeholder="Title" rows="1"></textarea>
+
+            <textarea
+                class="mt-4 h-48 resize-y w-full border-transparent focus:border-transparent focus:ring-transparent placeholder:text-gray-400"
+                name="body" placeholder="Content here...">{{ old('body') }}</textarea>
         </form>
     </div>
 </x-app-layout>
-<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-<script>
-    tinymce.init({
-        selector: 'textarea#body',
-        plugins: 'autoformat code',
-        toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify',
-        menubar: false,
-        statusbar: false,
-    });
-</script>
