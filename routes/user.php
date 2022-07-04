6382b725a2bbb95e11d5fa12bbe29374e44831b0
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-04 14:22:14 +0800

    Update user.php

--- routes/user.php
+++ routes/user.php
@@ -3,11 +3,15 @@
 use App\Http\Controllers\User\WrittenPostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\PreviewPostController;
 
 Route::middleware('auth')->group(function () {
 
     Route::get('/my-posts', [WrittenPostController::class, 'index'])
         ->name('my-posts');
+
+    Route::get('/my-posts/{post:slug}/preview', [PreviewPostController::class, 'show'])
+        ->name('my-posts.preview');
 
     Route::get('/write-post', [WrittenPostController::class, 'create'])
         ->name('write-post');
