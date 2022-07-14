3989499a95bcc614dec7b42c5283ebaebd065c57
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-14 04:10:38 +0000

    Add EasyMDE to global and function initEasyMDE

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,22 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+import EasyMDE from 'easymde';
+
+window.EasyMDE = EasyMDE;
+
+window.initEasyMDE = (elementId) => {
+    const textArea = document.getElementById(elementId);
+
+    const easymde = new EasyMDE({
+        element: textArea,
+        spellChecker: false
+    });
+
+    easymde.codemirror.on('change', () => {
+        textArea.value = easymde.value();
+    });
+
+    return easymde;
+}
