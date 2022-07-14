269e07187078b0cda4b1a9282141b6d2b214a937
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-14 06:16:50 +0000

    Remove blade config in plugin

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,5 @@ export default defineConfig({
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        },
     ],
 });
