a55c9ab6877ee866ef13d9b415a954a0fe45afb3
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-16 10:29:26 +0000

    Fix display on draft post

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,8 @@
                 <h1>{{ $post->title }}</h1>
                 <p class="text-gray-600">
                     Posted by {{ $post->user->name }}
-                    <span class="block">Published at {{ $post->published_at->format('M d, Y') }}</span>
+                    <span class="block">{{ $post->isPublished() ? 'Published at' : 'Draft' }}
+                        {{ $post->published_at?->format('M d, Y') }}</span>
                 </p>
                 <p class="mt-4">
                     {!! Str::markdown($post->body) !!}
