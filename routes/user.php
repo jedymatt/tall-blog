51982eb9785b4899e432492e59f39c0af13b2995
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-20 12:28:35 +0000

    Remove nonexistent controller import

--- routes/user.php
+++ routes/user.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\User\PreviewPostController;
 use App\Models\Post;
 use App\Services\PostService;
 use Illuminate\Http\Request;
